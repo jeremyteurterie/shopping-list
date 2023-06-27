633e0bd2a96c24a10105a6ec0e19ac39ebf378ef
Jeremy <jeremy.teurterie@gmail.com>
2023-06-27 14:54:27 +0700

    :sparkles: Feat: update item & reset state

--- script.js
+++ script.js
@@ -23,6 +23,15 @@ function onAddItemSubmit(e) {
     return;
   }
 
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('edit-mode');
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove('edit-mode');
+    itemToEdit.remove();
+    isEditMode = false;
+  }
+
   addItemToDOM(newItem);
 
   addItemToStorage(newItem);
@@ -138,6 +147,8 @@ function filterItems(e) {
 }
 
 function checkUI() {
+  itemInput.value = '';
+
   const items = itemList.querySelectorAll('li');
 
   if (items.length === 0) {
@@ -147,6 +158,11 @@ function checkUI() {
     clearBtn.style.display = 'block';
     itemFilter.style.display = 'block';
   }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = '#333';
+
+  isEditMode = false;
 }
 
 function init() {
